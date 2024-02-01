ab81cc6616a504a8eb9ebc351d2ae97e9426bf72
miahro <mikko.ahro@gmail.com>
2024-02-01 13:48:55 +0200

    fix year order and default in GroupManagementPage

--- frontend/src/components/GroupManagementPage/GroupManagementPage.js
+++ frontend/src/components/GroupManagementPage/GroupManagementPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import Paper from '@material-ui/core/Paper'
@@ -24,55 +24,56 @@ const ConfigurationSelectWrapper = ({ label, children }) => (
   </div>
 )
 
-class GroupManagementPage extends React.Component {
-  async componentDidMount() {
-    try {
-      const fetchedConfiguration = await configurationService.getAll()
+const GroupManagementPage = (props) => {
+  const { setUsers, setGroups, fetchTopics, setConfigurations, setError } =
+    props
 
-      const fetchedGroups = await groupManagementService.get()
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedConfiguration = await configurationService.getAll()
+        const fetchedGroups = await groupManagementService.get()
+        const fetchedUsers = await userService.get()
 
-      const fetchedUsers = await userService.get()
-
-      this.props.setUsers(fetchedUsers)
-
-      this.props.setGroups(fetchedGroups)
-      await this.props.fetchTopics()
-      this.props.setConfigurations(fetchedConfiguration.configurations)
-    } catch (e) {
-      this.props.setError('Some error happened')
+        setUsers(fetchedUsers)
+        setGroups(fetchedGroups)
+        await fetchTopics()
+        setConfigurations(fetchedConfiguration.configurations.reverse())
+      } catch (err) {
+        setError('Some error happened')
+      }
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <h2>Administration</h2>
-        <h3>Group Management</h3>
+    fetchData()
+  }, [setUsers, setGroups, fetchTopics, setConfigurations, setError])
 
-        <ConfigurationSelectWrapper label="Select configuration">
-          <ConfigurationSelect props={this.props} />
-        </ConfigurationSelectWrapper>
+  return (
+    <div>
+      <h2>Administration</h2>
+      <h3>Group Management</h3>
 
-        <Paper elevation={2} style={{ padding: '1rem 1.5rem' }}>
-          <h4>Create group</h4>
-          <GroupCreationForm />
-        </Paper>
+      <ConfigurationSelectWrapper label="Select configuration">
+        <ConfigurationSelect props={props} />
+      </ConfigurationSelectWrapper>
 
-        <p />
+      <Paper elevation={2} style={{ padding: '1rem 1.5rem' }}>
+        <h4>Create group</h4>
+        <GroupCreationForm />
+      </Paper>
 
-        <h4>Created groups</h4>
+      <p />
 
-        <GroupManagementForm />
-      </div>
-    )
-  }
+      <h4>Created groups</h4>
+
+      <GroupManagementForm />
+    </div>
+  )
 }
-
 const mapStateToProps = (state) => ({
   topics: state.topicListPage.topics,
   configurations: state.configurationPage.configurations,
   groups: state.groupPage.groups,
-  users: state.groupPage.users
+  users: state.groupPage.users,
 })
 
 const mapDispatchToProps = {
@@ -82,10 +83,7 @@ const mapDispatchToProps = {
   setUsers: groupManagementActions.setUsers,
   setError: notificationActions.setError,
   setSuccess: notificationActions.setSuccess,
-  fetchTopics: topicListPageActions.fetchTopics
+  fetchTopics: topicListPageActions.fetchTopics,
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(GroupManagementPage)
+export default connect(mapStateToProps, mapDispatchToProps)(GroupManagementPage)
