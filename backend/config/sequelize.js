3584fca804353a82fbbd6428326ceff60eacd88f
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-02-21 13:10:22 +0200

    Changes for running npm db scripts on OpenShift

---
+++ backend/config/sequelize.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "database": "dev_postgres",
+    "port": "5432",
+    "host": "localhost",
+    "dialect": "postgres",
+    "operatorsAliases": false,
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 10000,
+      "idle": 300000000
+    }
+  }, "staging": {
+    "username": process.env.DB_USERNAME,
+    "database": process.env.DB_NAME,
+    "port": process.env.DB_PORT,
+    "host": process.env.DB_HOST,
+    "password": process.env.DB_PASSWORD,
+    "dialect": "postgres",
+    "operatorsAliases": false,
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 10000,
+      "idle": 300000000
+    }
+  }
+}
