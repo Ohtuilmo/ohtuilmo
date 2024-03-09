42f35ca63e5756572ecb5898f586373da3b3c573
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-03-09 11:19:23 +0200

    Fix config file for Sequelize CLI

---
+++ backend/config/sequelize.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL
+  },
+  test: {
+    url: process.env.DATABASE_URL
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    ssl: true,
+    dialectOptions: {
+      ssl: true
+    }
+  }
+}
