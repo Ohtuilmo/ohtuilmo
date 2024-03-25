45fe5c79e1d8c4db9e8ab1d080d20291545a65b8
miahro <mikko.ahro@gmail.com>
2024-03-25 16:19:35 +0200

    change better fake data

--- backend/controllers/groupSprintSummary.js
+++ backend/controllers/groupSprintSummary.js
@@ -6,22 +6,42 @@ const group = require('../models/group')
 
 
 const fakeData =
-  [ {
-    1: [
-      { 'Joonatan Huang': 180 },
-      { 'Mikko Ahro': 100 }]
-  },
-  {
-    2: [
-      { 'Joonatan Huang': 200 },
-      { 'Mikko Ahro': 20 }]
-  },
-  {
-    3: [
-      { 'Joonatan Huang': 100 },
-      { 'Mikko Ahro': 10 }]
-  }
+  [
+    {
+      '1': [
+        { 'Joonatan Huang': 120 },
+        { 'Mikko Ahro': 150 },
+        { 'Ella Virtanen': 130 },
+        { 'Leo Niemi': 140 },
+        { 'Noora Laine': 110 },
+        { 'Sofia Heikkilä': 125 },
+        { 'Antti Korhonen': 135 }
+      ]
+    },
+    {
+      '2': [
+        { 'Joonatan Huang': 90 },
+        { 'Mikko Ahro': 180 },
+        { 'Ella Virtanen': 120 },
+        { 'Leo Niemi': 160 },
+        { 'Noora Laine': 105 },
+        { 'Sofia Heikkilä': 135 },
+        { 'Antti Korhonen': 140 }
+      ]
+    },
+    {
+      '3': [
+        { 'Joonatan Huang': 200 },
+        { 'Mikko Ahro': 160 },
+        { 'Ella Virtanen': 150 },
+        { 'Leo Niemi': 170 },
+        { 'Noora Laine': 120 },
+        { 'Sofia Heikkilä': 130 },
+        { 'Antti Korhonen': 125 }
+      ]
+    }
   ]
+
 
 timeLogsRouter.get('/:id', checkLogin, async (req, res) => {
   const id = req.params.id
