d16e094e6f619932bf89a456b9a2be397523df00
regularmute <93884347+Regularmute@users.noreply.github.com>
2024-03-26 13:00:32 +0200

    create component for delete confirmation window

---
+++ frontend/src/components/common/ConfirmationDialog.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Button from "@material-ui/core/Button"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogTitle from "@material-ui/core/DialogTitle"
+
+const ConfirmationDialog = (props) => {
+  const { title, children, open, setOpen, onConfirm } = props
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      className="confirmation-dialog"
+    >
+      <DialogTitle id="confirmation-dialog">{title}</DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions>
+        <Button
+          id="confirmation-dialog-no-button"
+          variant="contained"
+          onClick={() => setOpen(false)}
+          color="secondary"
+        >
+          No
+        </Button>
+        <Button
+          id="confirmation-dialog-yes-button"
+          variant="contained"
+          onClick={() => {
+            setOpen(false)
+            onConfirm()
+          }}
+          color="default"
+        >
+          Yes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default ConfirmationDialog
