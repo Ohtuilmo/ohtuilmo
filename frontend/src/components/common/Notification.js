58642206896582815e04e282de4e66d0775f15da
miahro <mikko.ahro@gmail.com>
2024-04-09 10:18:20 +0300

    prevent deleting sprints with existing time logs

--- frontend/src/components/common/Notification.js
+++ frontend/src/components/common/Notification.js
@@ -40,7 +40,8 @@ const Notification = ({
       >
         <SnackbarContent
           style={style}
-          message={message}
+          //message={message}
+          message={<span data-testid="notification-message">{message}</span>}
           headlineMapping={{
             body1: 'div',
             body2: 'div'
