268756a8897d7662acd13437d72ebd3a4ace6d4e
Antti Aarnio <laborantti.antti@gmail.com>
2024-04-10 15:07:08 +0300

    adding ability to wait for elements to fully load by adding timeouts to timelogs tests

--- frontend/e2e/cypress/integration/timeLogs.spec.js
+++ frontend/e2e/cypress/integration/timeLogs.spec.js
@@ -320,9 +320,9 @@ describe('Time logs & sprints', () => {
     cy.get('.description', { timeout: 5000 }).type('1234')
     cy.get('.submit-button').click()
 
-      cy.get('.input-container').contains('Description must be at least 5 characters')
-      cy.get('#timelog-rows').should('not.contain', '1234')
-    })
+    cy.get('.input-container').contains('Description must be at least 5 characters')
+    cy.get('#timelog-rows').should('not.contain', '1234')
+  })
 
     it('asks for confirmation before deleting a sprint', () => {
       cy.get('#hamburger-menu-button')
@@ -359,69 +359,14 @@ describe('Time logs & sprints', () => {
           cy.contains('Sprint Dashboard').click()
         })
 
-      cy.get('.sprints-container')
-        .find('[id^="sprint-remove-button-"]')
-        .click({ multiple: true })
-        .then(() => {
-          cy.get('.notification').should('exist')
-          cy.get('[data-testid="notification-message"]').should('contain', 'Sprint has time logs, cannot delete.')
-        }
-        )
-    })
-
-
-    it('remove sprints, should not display sprints or time logs', () => {
-      cy.get('#hamburger-menu-button')
-      .click()
+    cy.get('.sprints-container', { timeout: 5000 })
+      .find('[id^="sprint-remove-button-"]')
+      .click({ multiple: true })
       .then(() => {
-        cy.contains('Time Log').click()
+        cy.get('.notification').should('exist')
+        cy.get('[data-testid="notification-message"]').should('contain', 'Sprint has time logs, cannot delete.')
       })
-
-    cy.get(':nth-child(1) > .timelogs-description')
-      .invoke('text')
-      .as('removedLogDescription')
-
-    cy.get('#timelog-rows > :nth-child(1)')
-      .find('[id^="timelog-remove-button-"]')
-      .click()
-
-    cy.get('.confirmation-dialog').should(
-      'contain',
-      'Delete this time log? It cannot be restored.'
-    )
-    cy.get('.confirmation-dialog').find('#confirmation-dialog-yes-button').click()
-
-    cy.get('@removedLogDescription').then((removedLogDescription) => {
-      cy.get('#timelog-rows > :nth-child(1) > .timelogs-description').should(
-        'not.contain',
-        removedLogDescription
-      )
-    })
-
-    cy.get('#timelog-rows').children().should('have.length', 1)
-
-
-      cy.get('#hamburger-menu-button')
-        .click()
-        .then(() => {
-          cy.contains('Sprint Dashboard').click()
-        })
-      cy.get('.sprints-container')
-        .find('[id^="sprint-remove-button-"]')
-        .click({ multiple: true })
-        .then(() =>
-          cy.get('#app-content').should('not.contain', '.sprint-list-container')
-        )
-
-      cy.get('.sprints-container')
-        .find('[id^="sprint-remove-button-"]')
-        .click({ multiple: true })
-        .then(() => {
-          cy.get('.notification').should('exist')
-          cy.get('[data-testid="notification-message"]').should('contain', 'Sprint has time logs, cannot delete.')
-        }
-        )
-    })
+  })
 
 
   it('remove sprints, should not display sprints or time logs', () => {
@@ -665,7 +610,7 @@ describe('Time logs & sprints', () => {
         .get('#timelogs-chart-sprint')
         .should('not.exist')
       cy
-        .get('#timelogs-placeholder-sprint')
+        .get('#timelogs-placeholder-sprint', { timeout: 5000 })
         .should('exist')
         .and('be.visible')
         .and('contain', 'There are no time logs available for this sprint.')
