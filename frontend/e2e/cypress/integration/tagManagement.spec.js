f4b7899fb31b2e70a43a9d2ffba671468d92c9e1
miahro <mikko.ahro@gmail.com>
2024-04-15 14:39:49 +0300

    add tests to admin tagManagement

---
+++ frontend/e2e/cypress/integration/tagManagement.spec.js
@@ -0,0 +1,66 @@
+/* eslint-disable */
+
+describe('Tag management', () => {
+
+  beforeEach(() => {
+    cy.loginAsAdmin()
+    cy.visit('/')
+  })
+
+  it('tag management page opens', () => {
+    cy.get('#hamburger-menu-button')
+      .click()
+      .then(() => {
+        cy.contains('Tag management').click()
+      })
+    cy.contains('Add new tag').should('exist')
+
+  })
+  it('admin can add a new tag', () => {
+    cy.get('#hamburger-menu-button')
+      .click()
+      .then(() => {
+        cy.contains('Tag management').click()
+      })
+    cy.get('#tagTitle').type('New tag')
+    cy.get('.button').click()
+    cy.contains('New tag').should('exist')
+  })
+  it('duplicate tag cannot be added', () => {
+    cy.get('#hamburger-menu-button')
+      .click()
+      .then(() => {
+        cy.contains('Tag management').click()
+      })
+    cy.get('#tagTitle').type('New tag')
+    cy.get('.button').click()
+    cy.get('#tagTitle').clear().type('New tag')
+    cy.get('.button').click()
+    cy.contains('Tag already exists').should('exist')
+  })
+  it('too short tag cannot be added', () => {
+    cy.get('#hamburger-menu-button')
+      .click()
+      .then(() => {
+        cy.contains('Tag management').click()
+      })
+    cy.get('#tagTitle').type('NT')
+    cy.get('.button').click()
+    cy.contains('Title must be at least 3 characters.').should('exist')
+  })
+  it('tag can be deleted', () => {
+    cy.get('#hamburger-menu-button')
+      .click()
+      .then(() => {
+        cy.contains('Tag management').click()
+      })
+    cy.get('#tagTitle').type('New tag')
+    cy.get('.delete-tag-button').click()
+    cy.contains('New tag').should('not.exist')
+  })
+
+  // missing test: tag associated with existing time log cannot be deleted  
+  // is not tested. This is because the test would require a time log to be created and associated with a tag.'
+  // and cannot be done before adding tag to time logs front end is implemented.
+  // all backend functionality is there
+})
