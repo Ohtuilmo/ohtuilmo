94985367503816d156c6f6b9ec1ed643c88cd06b
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-04-16 11:45:07 +0300

    Update frontend to use node 20 and Vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default defineConfig(({ command }) => {
+  if (command === 'serve') { // when running dev server
+    return {
+      base: '/',
+      server: {
+        port: 3000,
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3001',
+            changeOrigin: true,
+          },
+        },
+      },
+      plugins: [react(), reactRefresh()],
+      define: {
+        global: 'window'
+      },
+    }
+  } else { // when building app
+    return {
+      base: '/projekti/',
+      define: {
+        global: 'window'
+      },
+    }
+  }
+})
