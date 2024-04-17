7f20e812b6d7ca140d4b5a7f15f21fe43a366d3c
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-04-17 11:00:41 +0300

    Fix eslint error

--- frontend/src/services/timeLogs.js
+++ frontend/src/services/timeLogs.js
@@ -13,6 +13,7 @@ const getGroupSprintSummary = async () => {
     })
     return response.data
   } catch (error) {
+    console.error('error in getGroupSprintSummary', error.response.data.error)
     throw error
   }
 }
