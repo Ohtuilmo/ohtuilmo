471750bc98a345dd73d6b97b49b214bb8a02fbe2
miahro <mikko.ahro@gmail.com>
2024-04-22 22:09:58 +0300

    improve tag view in time logs page

--- frontend/src/components/TimeLogsPage/TimeLogRow.jsx
+++ frontend/src/components/TimeLogsPage/TimeLogRow.jsx
@@ -11,45 +11,40 @@ export const TimeLogRow = ({ log, handleDelete }) => {
   const [confirmOpen, setConfirmOpen] = useState(false)
 
   return (
-    <div className="timelogs-table-row">
-      <div className="timelogs-date-and-time">
-        <p>
-          {hours}:{minutes}
-        </p>
-        <p>{log.date}</p>
-      </div>
-      <div className="timelogs-description">
-        <p>{log.description}</p>
+    <div className="timelogs-row-container">
+      <div className="timelogs-table-row">
+        <div className="timelogs-date-and-time">
+          <p>
+            {hours}:{minutes}
+          </p>
+          <p>{log.date}</p>
+        </div>
+        <div className="timelogs-description">
+          <p>{log.description}</p>
+        </div>
+        <IconButton
+          id={`timelog-remove-button-${log.id}`}
+          className="timelogs-remove-button"
+          style={{ padding: '0 12px' }}
+          disableRipple
+          onClick={() => setConfirmOpen(true)}
+        >
+          <DeleteOutlineRounded />
+        </IconButton>
+        <ConfirmationDialog
+          title="Delete Time Log?"
+          open={confirmOpen}
+          setOpen={setConfirmOpen}
+          onConfirm={handleDelete}
+        >
+          Delete this time log? It cannot be restored.
+        </ConfirmationDialog>
       </div>
       <div className="timelogs-tags-row">
         {log.tags.map((tag) => (
           <Chip key={tag} label={tag} className="timelogs-tag" size="small" />
         ))}
       </div>
-      {/* <div className="timelogs-tags">
-        {log.tags.map((tag) => (
-          <p key={tag} className="timelogs-tag">
-            {tag}
-          </p>
-        ))}
-      </div> */}
-      <IconButton
-        id={`timelog-remove-button-${log.id}`}
-        className="timelogs-remove-button"
-        style={{ padding: '0 12px' }}
-        disableRipple
-        onClick={() => setConfirmOpen(true)}
-      >
-        <DeleteOutlineRounded />
-      </IconButton>
-      <ConfirmationDialog
-        title="Delete Time Log?"
-        open={confirmOpen}
-        setOpen={setConfirmOpen}
-        onConfirm={handleDelete}
-      >
-        Delete this time log? It cannot be restored.
-      </ConfirmationDialog>
     </div>
   )
 }
