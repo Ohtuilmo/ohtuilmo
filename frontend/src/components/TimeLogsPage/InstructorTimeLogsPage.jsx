bd0bd5f6f831bdb29a3e676bc8b3d2adc256c003
regularmute <93884347+Regularmute@users.noreply.github.com>
2024-04-25 15:38:32 +0300

    add student-selector to display their group's name

--- frontend/src/components/TimeLogsPage/InstructorTimeLogsPage.jsx
+++ frontend/src/components/TimeLogsPage/InstructorTimeLogsPage.jsx
@@ -1,8 +1,65 @@
+import React, { useEffect, useState } from 'react'
+
+import { TimeLogsSelectForm } from './TimeLogsSelectForm'
+
 import { withRouter } from 'react-router-dom'
 
+import userService from '../../services/user'
+import groupManagementService from '../../services/groupManagement'
+
 const InstructorTimeLogsPage = () => {
+  const [selectedStudent, setSelectedStudent] = useState(null)
+  const [allStudents, setAllStudents] = useState([])
+  const [selectedGroup, setSelectedGroup] = useState(null)
+
+  useEffect(() => {
+      const fetchAllStudents = async () => {
+        try {
+          const allStudents = await userService.get()
+          setAllStudents(allStudents)
+        } catch (error) {
+          console.error(
+            'Error fetching students:',
+            error.message,
+            ' / ',
+            error.response.data.error
+          )
+          notificationActions.setError(error.response.data.error)
+        }
+      }
+
+      fetchAllStudents()
+    }, [])
+
+  useEffect(() => {
+    const fetchGroup = async () => {
+      try {
+        const group = await groupManagementService.getByStudent(selectedStudent)
+        setSelectedGroup(group)
+      } catch (error) {
+        console.error(
+          'Error fetching group:',
+          error.message,
+          ' / ',
+          error.response.data.error
+        )
+        notificationActions.setError(error.response.data.error)
+      }
+    }
+
+    fetchGroup()
+    console.log(selectedGroup)
+  }, [selectedStudent])
+
   return (
-      <div>Hello Instructor!</div>
+    <div>
+      <TimeLogsSelectForm
+        students={allStudents}
+        handleStudentChange={setSelectedStudent}
+      />
+      <div>{selectedStudent}</div>
+      {selectedGroup && <div>{selectedGroup.groupName}</div>}
+     </div>
   )
 }
 
