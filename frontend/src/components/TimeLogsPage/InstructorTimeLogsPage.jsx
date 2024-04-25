f62d23fc99bc25962a7e96317cfb1f741be30735
regularmute <93884347+Regularmute@users.noreply.github.com>
2024-04-25 16:44:09 +0300

    admin can see a student's recorded time logs

--- frontend/src/components/TimeLogsPage/InstructorTimeLogsPage.jsx
+++ frontend/src/components/TimeLogsPage/InstructorTimeLogsPage.jsx
@@ -11,6 +11,7 @@ import timeLogsService from '../../services/timeLogs'
 const InstructorTimeLogsPage = () => {
   const [selectedStudent, setSelectedStudent] = useState(null)
   const [allStudents, setAllStudents] = useState([])
+  const [allLogs, setAllLogs] = useState(null)
   const [selectedGroup, setSelectedGroup] = useState(null)
 
   useEffect(() => {
@@ -29,7 +30,26 @@ const InstructorTimeLogsPage = () => {
         }
       }
 
-      fetchAllStudents()
+      const fetchAllLogs = async () => {
+        try {
+          const allLogs = await timeLogsService.getTimeLogs()
+          setAllLogs(allLogs)
+        } catch (error) {
+          console.error(
+            'Error fetching logs:',
+            error.message,
+            ' / ',
+            error.response.data.error
+          )
+          notificationActions.setError(error.response.data.error)
+        }
+      }
+
+      const fetchData = async () => {
+        await fetchAllStudents()
+        await fetchAllLogs()
+      }
+      fetchData()
     }, [])
 
   useEffect(() => {
@@ -51,6 +71,10 @@ const InstructorTimeLogsPage = () => {
     fetchGroup()
   }, [selectedStudent])
 
+  const isLogs = (logs) => logs && logs.length > 0
+  const logsByStudent =
+    isLogs(allLogs) && allLogs.filter((log) => log.studentNumber === selectedStudent)
+
   return (
     <div>
       <div>
@@ -61,6 +85,18 @@ const InstructorTimeLogsPage = () => {
         />
         <div>{selectedStudent}</div>
         {selectedGroup && <div>{selectedGroup.groupName}</div>}
+      </div>
+      <div id='timelog-rows'>
+        {isLogs(logsByStudent) &&
+          logsByStudent.map((log) => (
+            <TimeLogRow
+              key={log.id}
+              log={log}
+            />
+          ))}
+        {!isLogs(logsByStudent) && (
+          <p>No logs yet :&#40;</p>
+        )}
       </div>
    </div>
   )
