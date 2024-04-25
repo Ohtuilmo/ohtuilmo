415c8487fffa9f5ec90909262241ba0b0bdbf6f3
regularmute <93884347+Regularmute@users.noreply.github.com>
2024-04-25 16:40:23 +0300

    backend controller can fetch all logs for admin

--- backend/controllers/timeLogs.js
+++ backend/controllers/timeLogs.js
@@ -101,8 +101,80 @@ const fetchFromDb = async (studentNumber) => {
   }
 }
 
+const fetchAllFromDb = async () => {
+  try {
+    const rawLogs = await db.TimeLog.findAll({
+      attributes: [
+        ['id', 'id'],
+        ['date', 'date'],
+        ['minutes', 'minutes'],
+        ['student_number', 'studentNumber'],
+        'description',
+        [Sequelize.literal('sprint.sprint'), 'sprint'],
+        [Sequelize.literal('group_id'), 'groupId'],
+      ],
+      include: [
+        {
+          model: db.Sprint,
+          as: 'sprint',
+          attributes: [],
+        },
+        {
+          model: db.Tag,
+          as: 'tags',
+          attributes: ['title'],
+          through: { attributes: [] },
+        },
+      ],
+      raw: true,
+      order: [
+        ['date', 'DESC'],
+        ['created_at', 'DESC'],
+      ],
+    })
+
+    const timeLogMap = new Map()
+    rawLogs.forEach((log) => {
+      const {
+        id,
+        studentNumber,
+        sprint,
+        date,
+        minutes,
+        description,
+        groupId,
+        ...rest
+      } = log
+      const formattedDate = new Date(date).toISOString().slice(0, 10)
+      const timeLog = timeLogMap.get(id) || {
+        id,
+        studentNumber,
+        sprint,
+        date: formattedDate,
+        minutes,
+        description,
+        groupId,
+        tags: [],
+      }
+      const tag = rest['tags.title']
+      if (tag) {
+        timeLog.tags.push(tag)
+      }
+      timeLogMap.set(id, timeLog)
+    })
+    return Array.from(timeLogMap.values())
+  } catch (error) {
+    console.error('Error fetching time logs:', error)
+    throw new Error('Error fetching time logs.')
+  }
+}
+
 timeLogsRouter.get('/', checkLogin, async (req, res) => {
   try {
+    if (req.user.admin) {
+      const timeLogs = await fetchAllFromDb()
+      return res.status(200).json(timeLogs)
+    }
     const user_id = req.user.id
     const timeLogs = user_id ? await fetchFromDb(user_id) : []
     return res.status(200).json(timeLogs)
