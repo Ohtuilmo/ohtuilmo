fdf43648a8323f6bbee4101463728918c71a6227
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2024-04-26 15:54:44 +0300

    Add a peerReview to test seeders, fix PeerReviewAnswer for StudentViewGroupAnswers

--- frontend/src/components/InstructorPage/StudentViewGroupAnswers.jsx
+++ frontend/src/components/InstructorPage/StudentViewGroupAnswers.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import { extractCallingName } from '../../utils/functions'
+
 import './InstructorPage.css'
+
+const PeerReviewAnswer = ({ answers, questionHeader, student }) => {
+  const studentName = `${extractCallingName(student.first_names)} ${student.last_name}`
+
+  // It's so dumb to use the student's name instead of id here, but this is
+  // all over the codebase so prolly requires quite a bit of work to fix.
+  const otherStudentsAnswers = answers.filter(
+    answer => `${extractCallingName(answer.student.first_names)} ${answer.student.last_name}` !== studentName)
+
+  return (
+    <div>
+      <h3>{questionHeader}</h3>
+      {otherStudentsAnswers.map((answer, index) => (
+        <p key={index}>From {`${answer.student.first_names} ${answer.student.last_name}`}: {answer.peers[studentName]}</p>
+      ))}
+    </div>
+  )
+}
 
 const RadioAnswer = ({ answers, questionHeader, student }) => {
   const allPeers = answers.reduce((acc, answer) => {
@@ -83,15 +103,15 @@ const StudentViewGroupAnswers = ({ answers }) => {
               )
             } else if (question.type === 'peerReview') {
               return (
-                <div key={questionIndex}>
-                  <h3>{question.questionHeader}</h3>
-                  {question &&
-                    Object.entries(question.peers).map(([peer, review]) => (
-                      <p key={peer}>
-                        {peer}: {review}
-                      </p>
-                    ))}
-                </div>
+                <PeerReviewAnswer
+                  key={questionIndex}
+                  answers={answers.map(roundAnswer => ({
+                    student: roundAnswer.student,
+                    peers: roundAnswer.answer_sheet[questionIndex].peers
+                  }))}
+                  questionHeader={question.questionHeader}
+                  student={studentAnswer.student}
+                />
               )
             } else {
               return (
