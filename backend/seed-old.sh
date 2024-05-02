fbbc1c826b450e9cd925eeb901575f26110841c5
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-05-02 09:59:05 +0300

    Delete unnecessary files

--- backend/seed-old.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sequelize="./node_modules/.bin/sequelize"
-
-$sequelize db:seed --seed 20190221035516-page-access-seeds.js
-$sequelize db:seed --seed 20190226153149-group-management-seeds.js
-$sequelize db:seed --seed 20190424111842-topic-for-active-tests.js
-$sequelize db:seed --seed 20190502093218-additional-configuration.js
