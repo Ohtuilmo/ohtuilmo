707e4f524c3aa7d9c4d0a9f1f465b2773eb0a12e
Amanda <amandahamynen@users.noreply.github.com>
2025-06-08 12:42:55 +0300

    Refactor TimeLogRow.jsx to handle less logic

--- frontend/src/components/TimeLogsPage/TimeLogRow.jsx
+++ frontend/src/components/TimeLogsPage/TimeLogRow.jsx
@@ -3,15 +3,13 @@ import { minutesToFormattedHoursAndMinutes } from '../../utils/functions'
 import { DeleteOutlineRounded } from '@material-ui/icons'
 import { IconButton, Chip } from '@material-ui/core'
 import ConfirmationDialog from '../common/ConfirmationDialog'
-import Button from '@material-ui/core/Button'
+import Checkbox from '@material-ui/core/Checkbox'
 
 import './TimeLogsPage.css'
 
-export const TimeLogRow = ({ log, handleDelete, handleMoveToPreviousSprint, handleMoveToNextSprint, role }) => {
+export const TimeLogRow = ({ log, handleDelete, handleTimeLogCheck, isChecked, user }) => {
   const { hours, minutes } = minutesToFormattedHoursAndMinutes(log.minutes)
   const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false)
-  const [previousMoveConfirmOpen, setPreviousMoveConfirmOpen] = useState(false)
-  const [nextMoveConfirmOpen, setNextMoveConfirmOpen] = useState(false)
 
   const dateObj = new Date(log.date)
   const formattedDate = dateObj.toLocaleDateString('fi-FI', {
@@ -20,51 +18,26 @@ export const TimeLogRow = ({ log, handleDelete, handleMoveToPreviousSprint, hand
     year: 'numeric',
   }).replace(/\./g, '/')
 
-  const instructorItems = () => {
-    return (
-      <div>
-        <Button
-            id={`timelog-move-button-previous-${log.id}`}
-            className="timelogs-move-button-previous"
-            style={{ padding: '0 12px' }}
-            disableRipple
-            onClick={() => setPreviousMoveConfirmOpen(true)}
-          >
-            Move to previous sprint
-          </Button>
-          <ConfirmationDialog
-            title="Move Time Log to previous sprint?"
-            open={previousMoveConfirmOpen}
-            setOpen={setPreviousMoveConfirmOpen}
-            onConfirm={handleMoveToPreviousSprint}
-          >
-            Move this time log to previos sprint?
-          </ConfirmationDialog>
-          <Button
-            id={`timelog-move-button-next-${log.id}`}
-            className="timelogs-move-button-next"
-            style={{ padding: '0 12px' }}
-            disableRipple
-            onClick={() => setNextMoveConfirmOpen(true)}
-          >
-            Move to next sprint
-          </Button>
-          <ConfirmationDialog
-            title="Move Time Log to next sprint?"
-            open={nextMoveConfirmOpen}
-            setOpen={setNextMoveConfirmOpen}
-            onConfirm={handleMoveToNextSprint}
-          >
-            Move this time log to next sprint?
-          </ConfirmationDialog>
-      </div>
-    )
-  }
-
-  const studentItems = () => {
-    return (
-      <div>
-        <IconButton
+  return (
+    <div className="timelogs-row-container">
+      <div className="timelogs-table-row">
+        {user.instructor && (
+          <Checkbox 
+            id={`timelog-checkbox-${log.id}`}
+            checked={isChecked}
+            onChange={handleTimeLogCheck}
+          />
+        )}
+        <div className="timelogs-date-and-time">
+          <p>
+            {hours}:{minutes}
+          </p>
+          <p>{formattedDate}</p>
+        </div>
+        <div className="timelogs-description">
+          <p>{log.description}</p>
+        </div>
+          <IconButton
             id={`timelog-remove-button-${log.id}`}
             className="timelogs-remove-button"
             style={{ padding: '0 12px' }}
@@ -81,24 +54,6 @@ export const TimeLogRow = ({ log, handleDelete, handleMoveToPreviousSprint, hand
           >
             Delete this time log? It cannot be restored.
           </ConfirmationDialog>
-      </div>
-    )
-  }
-
-  return (
-    <div className="timelogs-row-container">
-      <div className="timelogs-table-row">
-        <div className="timelogs-date-and-time">
-          <p>
-            {hours}:{minutes}
-          </p>
-          <p>{formattedDate}</p>
-        </div>
-        <div className="timelogs-description">
-          <p>{log.description}</p>
-        </div>
-        {role === 'instructor' ? instructorItems() : <></>}
-        {role === 'student' ? studentItems() : <></>}
       </div>
       <div className="timelogs-tags-row">
         {log.tags.map((tag) => (
