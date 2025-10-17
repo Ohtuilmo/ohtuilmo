e1a4429041fa6b3ab5ca26c2da3638c8debe3dac
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-10-17 18:35:15 +0300

    seed matti luukkainen as admin to fix fakeshibbo middleware on fresh install

---
+++ backend/seeders/20251017152454-add-admin-matti-luukkainen.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const newUsers = [
+  {
+    student_number: '011120775',
+    username: 'mluukkai',
+    first_names: 'Matti',
+    last_name: 'Luukkainen',
+    email: 'eiainakaanluukkaisensäpö@example.com',
+    admin: true,
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+]
+
+module.exports = {
+  up: async (query) => {
+    await query.bulkInsert('users', newUsers, {})
+  },
+
+  down: async (query) => {
+    await query.bulkDelete('users', { username: [ 'mluukkai' ] }, {})
+  }
+}
