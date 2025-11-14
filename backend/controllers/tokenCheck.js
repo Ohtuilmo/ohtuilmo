7a35078c0018bf090987037657a0c7a6e8f46e07
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-11-14 14:45:43 +0200

    added checkInstructor path and prefixed unused vars

--- backend/controllers/tokenCheck.js
+++ backend/controllers/tokenCheck.js
@@ -1,12 +1,16 @@
 const tokenCheckRouter = require('express').Router()
-const { checkLogin, checkAdmin } = require('../middleware')
+const { checkLogin, checkInstructor, checkAdmin } = require('../middleware')
 
-tokenCheckRouter.get('/login', checkLogin, (req, res) => {
+tokenCheckRouter.get('/login', checkLogin, (_req, res) => {
   res.status(200).json({ message: 'success' })
 })
 
-tokenCheckRouter.get('/admin', checkAdmin, (req, res) => {
+tokenCheckRouter.get('/admin', checkAdmin, (_req, res) => {
   res.status(200).json({ message: 'success' })
+})
+
+tokenCheckRouter.get("/instructor", checkInstructor, (_req, res) => {
+  res.status(200).json({ message: "success" })
 })
 
 module.exports = tokenCheckRouter
