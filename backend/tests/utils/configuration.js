9df1b076d20c59d2f855963f8c60c05240f4ff40
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-11-18 20:38:10 +0200

    typo fix in test utils

--- backend/tests/utils/configuration.js
+++ backend/tests/utils/configuration.js
@@ -1,14 +1,14 @@
-// const { loginAs, createTestUser, testAdmin, testUsers } = require('./login')
-const { createRegistrationQuestionSet } = require('./registrationQuestionSet')
+const { createTestRegistrationQuestionSet } = require('./registrationQuestionSet')
 
 const createTestConfiguration = async (db) => {
-  const registrationQuestionSetId = await createRegistrationQuestionSet(db)
+  const registrationQuestionSetId = await createTestRegistrationQuestionSet(db)
   await db.Configuration.create({
     name: 'Konfiguraatiolainen',
     content: { registrationQuestionSetId }
   })
+  return (await db.Configuration.findAll({}))[0].id
 }
 
-module.export = {
+module.exports = {
   createTestConfiguration
 }
