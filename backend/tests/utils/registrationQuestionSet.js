50d8f4eab2199de28aac2915e57902233417f21e
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-11-18 12:30:34 +0200

    added basic tests and test utils for registrationQuestionSets

---
+++ backend/tests/utils/registrationQuestionSet.js
@@ -0,0 +1,14 @@
+const createTestRegistrationQuestionSets = async (db) => {
+  await db.RegistrationQuestionSet.create({
+    name: 'Testikysymyksiä',
+    questions: [
+      { 'type': 'scale', 'question': 'Ossaakkonää koodata?' },
+      { 'type': 'text', 'question': 'Mitä muuta ossaat?' }
+    ]
+  })
+  return (await db.RegistrationQuestionSet.findAll({}))[0].id
+}
+
+module.exports = {
+  createTestRegistrationQuestionSets
+}
