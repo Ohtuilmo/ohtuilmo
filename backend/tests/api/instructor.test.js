5be0a24b5b7473789348df649db85f7b6a35d925
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-11-26 12:39:38 +0200

    added more instructor tests, changed login isInstructor return type

--- backend/tests/api/instructor.test.js
+++ backend/tests/api/instructor.test.js
@@ -3,43 +3,75 @@ const jwt = require('jsonwebtoken')
 
 const config = require('../../config/index')
 const { app, server, db } = require('../../index')
-const { loginAs, createTestUser, resetUsers, testUser } = require('../utils/login')
+const { loginAs, createTestUser, createTestUsers, resetUsers, testUser, testUsers } = require('../utils/login')
 const { createTestGroup, resetGroups, testInstructor } = require('../utils/groups')
 
 
-// jwt.verify(resHyPersonId.body.token, config.secret)
-
 describe('Instructor flag', () => {
   test('should be false for a new user without a group', async () => {
-    await resetUsers(db)
+    await createTestUser(db, testUser)
     const login = await loginAs(app, testUser.student_number)
     expect(jwt.verify(login.token, config.secret).instructor).toEqual(false)
   })
   test('should be false for an old user without a group', async () => {
+    await createTestUser(db, testUser)
     const login = await loginAs(app, testUser.student_number)
     expect(jwt.verify(login.token, config.secret).instructor).toEqual(false)
   })
   test('should be false for an user as a student in a group', async () => {
-    await resetUsers(db)
     await createTestGroup(db)
     const login = await loginAs(app, testUser.student_number)
 
     expect(jwt.verify(login.token, config.secret).instructor).toEqual(false)
   })
   test('should be true for an user that is an instructor for a group', async () => {
-    await resetUsers(db)
     await createTestGroup(db)
     const login = await loginAs(app, testInstructor.student_number)
 
     const decodedToken = jwt.verify(login.token, config.secret)
-    expect(decodedToken.instructor).not.toEqual(false)
-    expect(Object.keys(decodedToken.instructor)).toContain('id', 'name', 'instructorId', 'configurationId')
+    expect(decodedToken.instructor).toEqual(true)
+  })
+
+  test('should be true for instructor that has instructed for multiple times', async () => {
+    await createTestUsers(db, [...testUsers, testInstructor])
+    await createTestGroup(db, [testUsers[1], testUsers[2]], testInstructor, false)
+    await createTestGroup(db, [testUsers[0]], testInstructor, false)
+
+    const login = await loginAs(app, testInstructor.student_number)
+
+    const decodedToken = jwt.verify(login.token, config.secret)
+    expect(decodedToken.instructor).toEqual(true)
+  })
+
+  test('should be true for ongoing instructor while another project registration starts', async () => {
+    const anotherInstructor = JSON.parse(JSON.stringify(testInstructor))
+    anotherInstructor.student_number = 55555555555
+    await createTestUsers(db, [...testUsers, testInstructor, anotherInstructor])
+    await createTestGroup(db, [testUsers[1], testUsers[2]], testInstructor, false)
+    await createTestGroup(db, [testUsers[0]], anotherInstructor, false)
+
+    const login = await loginAs(app, testInstructor.student_number)
+
+    const decodedToken = jwt.verify(login.token, config.secret)
+    expect(decodedToken.instructor).toEqual(true)
+  })
+
+  test('should be false for instructor that has instructed in the past but not currently', async () => {
+    const anotherInstructor = JSON.parse(JSON.stringify(testInstructor))
+    anotherInstructor.student_number = 55555555555
+    await createTestUsers(db, [...testUsers, testInstructor, anotherInstructor])
+    await createTestGroup(db, [testUsers[1], testUsers[2]], testInstructor, false)
+    await createTestGroup(db, [testUsers[0]], anotherInstructor, false)
+
+    const login = await loginAs(app, testInstructor.student_number)
+
+    const decodedToken = jwt.verify(login.token, config.secret)
+    expect(decodedToken.instructor).toEqual(false)
   })
 
 
   beforeEach(async () => {
     await resetUsers(db)
-    await createTestUser(db, testUser)
     await resetGroups(db)
   })
 })
