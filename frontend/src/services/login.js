bb2a427837b77a983b070eb3c2fd3981ca5adc3b
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-12-04 11:04:13 +0200

    revert all having to do with login, I didn't understand shibbo

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -4,23 +4,6 @@ import { BACKEND_API_BASE } from '../utils/config'
 const url = `${BACKEND_API_BASE}/login`
 
 const login = async (credentials) => {
-  if (!credentials) {
-    // This needs to run before getting user from localStorage
-    // because otherwise dev fake_shibbo would not update
-    // role change
-    if (import.meta.env.MODE === 'development') {
-      const response = await axios.post(url, credentials)
-      return response.data
-    }
-
-    const savedUser = localStorage.getItem('loggedInUser')
-    if (savedUser) {
-      return JSON.parse(savedUser)
-    }
-
-    throw new Error('Login attempt failed, no credentials given')
-  }
-
   const response = await axios.post(url, credentials)
   return response.data
 }
