e39a7aaace337b6eceb1cbbf5791d8c96162d81f
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-12-04 08:45:54 +0200

    removed 401 /api/login logs from frontend

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -4,6 +4,16 @@ import { BACKEND_API_BASE } from '../utils/config'
 const url = `${BACKEND_API_BASE}/login`
 
 const login = async (credentials) => {
+  if (!credentials) {
+    // local dev uses backend fake_shibbo => 200 /api/login
+    // staging and production use shibbo => 401 /api/login
+    if (import.meta.env.MODE === "development") {
+      const response = await axios.post(url, credentials)
+      return response.data
+    }
+    throw new Error("Login attempt failed, no credentials given")
+  }
+
   const response = await axios.post(url, credentials)
   return response.data
 }
