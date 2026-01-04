00ab28ae0c0c8e4b7cf11de69413f076244cf8cc
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-01-04 13:29:35 +0200

    typed registationmanagement model

--- backend/models/registration_management.ts
+++ backend/models/registration_management.ts
@@ -1,4 +1,29 @@
-module.exports = (sequelize, Sequelize) => {
+import { Sequelize, Model, BuildOptions, Association } from "sequelize"
+import { Db } from "./index"
+
+export interface RegistrationManagement extends Model {
+  readonly id: number
+
+  readonly peer_review_open: boolean
+  readonly peer_review_round: 1 | 2
+
+  readonly project_registation_open: boolean
+  readonly project_registation_message: string
+  readonly project_registation_info: string
+
+  readonly topic_registation_open: boolean
+  readonly topic_registation_message: string
+
+  readonly summer_project: boolean
+  readonly summer_dates: any
+}
+
+export type RegistrationManagementStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): RegistrationManagement;
+  associate: (models: Required<Db>) => void
+}
+
+export default (sequelize: Sequelize, Sequelize: any) => {
   const Registration_management = sequelize.define('registration_management', {
     id: {
       type: Sequelize.INTEGER,
