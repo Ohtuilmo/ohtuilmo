9383e2156a8938e68c21f1824a3f8426e118edde
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-01-04 13:16:32 +0200

    typed topic model

--- backend/models/topic.ts
+++ backend/models/topic.ts
@@ -1,5 +1,35 @@
-module.exports = (sequelize, Sequelize) => {
-  const Topic = sequelize.define('topic', {
+import { Sequelize, Model, BuildOptions, Association } from "sequelize"
+import { Db } from "./index"
+import { User } from "./user"
+
+export interface Topic extends Model {
+  readonly id: number
+  readonly active: boolean
+  readonly content: TopicContent
+  readonly acronym: string
+  readonly secret_id: string
+
+  readonly createdAt: string
+  readonly updatedAt: string
+}
+
+export interface TopicContent {
+  email?: string
+  title?: string
+  description?: string
+  environment?: string
+  customerName?: string
+  additionalInfo?: string
+  specialRequests?: string
+}
+
+export type TopicStatic = typeof Model & {
+  new (values?: object, options?: BuildOptions): Topic;
+  associate: (models: Required<Db>) => void
+}
+
+export default (sequelize: Sequelize, Sequelize: any) => {
+  const Topic = <TopicStatic>sequelize.define('topic', {
     id: {
       type: Sequelize.INTEGER,
       primaryKey: true,
