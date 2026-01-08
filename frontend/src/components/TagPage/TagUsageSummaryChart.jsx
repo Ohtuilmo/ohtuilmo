8447495c2e2aae28ee0655b9829cfaa0024355fb
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-01-08 14:15:58 +0200

    Implement first working version of TagUsageSummaryChart

--- frontend/src/components/TagPage/TagUsageSummaryChart.jsx
+++ frontend/src/components/TagPage/TagUsageSummaryChart.jsx
@@ -1,3 +1,14 @@
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts'
+
 const TagUsageSummaryChart = ({allSprints, availableTags, tagData}) => {
   const data = allSprints.map(sprint => {
     const dataEntry = {name: sprint.sprint}
@@ -12,8 +23,31 @@ const TagUsageSummaryChart = ({allSprints, availableTags, tagData}) => {
   })
 
   return (
-    <>
-    </>
+    <div>
+    jotain2
+    <ResponsiveContainer>
+    jotain3
+    <LineChart
+      style={{ width: '100%', maxWidth: '700px', height: '500px', maxHeight: '500px', aspectRatio: 1.618 }}
+      responsive
+      data={data}
+      margin={{
+        top: 5,
+        right: 0,
+        left: 0,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis domain={[0, 150]} />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="tapaaminen" stroke="#8884d8" activeDot={{ r: 8 }} />
+      <Line type="monotone" dataKey="koodaus" stroke="#82ca9d" />
+    </LineChart>
+    </ResponsiveContainer>
+    </div>
   )
 }
 
