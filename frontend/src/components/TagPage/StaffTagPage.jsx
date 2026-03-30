c6f5cd531d9f811130fd3a96b092b19dfe7bd8ae
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-03-30 15:27:41 +0300

    Create tag page for instructors and admin
    Co-authored-by: Ville Sirviö <VSirvio@users.noreply.github.com>

---
+++ frontend/src/components/TagPage/StaffTagPage.jsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import LoadingSpinner from '../common/LoadingSpinner'
+import { Typography } from '@material-ui/core'
+import {
+  NotInGroupPlaceholder,
+  NoSprintsPlaceholder,
+} from '../common/Placeholders'
+import CheckboxMultiSelect from '../common/CheckboxMultiSelect'
+import TagUsageLineChart from './TagUsageLineChart'
+import TagUsageBarChart from './TagUsageBarChart'
+
+import sprintService from '../../services/sprints'
+import * as notificationActions from '../../reducers/actions/notificationActions'
+import tagsActions from '../../reducers/actions/tagActions'
+import './TagPage.css'
+
+const colourSet = [
+  '#9258c8',
+  '#3a75c4',
+  '#8c0032',
+  '#15bef0',
+  '#00b08c',
+  '#fca311',
+  '#009e60',
+  '#e5053a',
+  '#5bbf21',
+  '#00a39a',
+  '#e63375',
+  '#256ec7',
+  '#e95c55',
+  '#a3af07',
+]
+
+const StaffTagPage = (props) => {
+  const {
+    user,
+    group,
+    availableTags,
+    studentTags,
+    fetchAvailableTags,
+    fetchTagsByStudent,
+  } = props
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [allSprints, setAllSprints] = useState([])
+  const [selectedTags, setSelectedTags] = useState([])
+
+  const tagColors = availableTags.reduce((acc, tag, index) => {
+    acc[tag] = colourSet[index % colourSet.length]
+    return acc
+  }, {})
+
+  useEffect(() => {
+    const fetchSprints = async () => {
+      try {
+        const fetchedData = await sprintService.getSprints()
+        setAllSprints(fetchedData)
+      } catch (error) {
+        console.error(
+          'Error fetching sprints:',
+          error.message,
+          ' / ',
+          error.response.data.error,
+        )
+        notificationActions.setError(error.response.data.error)
+      }
+    }
+
+    const fetchData = async () => {
+      setIsLoading(true)
+      group?.id && (await fetchSprints())
+      await fetchTagsByStudent(user.studentNumber)
+      await fetchAvailableTags()
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, [fetchAvailableTags, fetchTagsByStudent, user.studentNumber, group?.id])
+
+  useEffect(() => {
+    setSelectedTags(availableTags)
+  }, [availableTags])
+
+  if (isLoading) return <LoadingSpinner />
+  if (!group) return <NotInGroupPlaceholder />
+  if (allSprints.length === 0) return <NoSprintsPlaceholder />
+
+  return (
+    <div className="tagpage-container">
+      <div className="tagpage-selection-container">
+        <Typography variant="h4">Tags</Typography>
+        <CheckboxMultiSelect
+          allItems={availableTags}
+          selectedItems={selectedTags}
+          setSelectedItems={setSelectedTags}
+        />
+      </div>
+      <div className="tagpage-charts-container">
+        {selectedTags.length === 0 ? (
+          <Typography variant="body1">No tags selected.</Typography>
+        ) : (
+          <>
+            <TagUsageBarChart
+              allTags={availableTags}
+              selectedTags={selectedTags}
+              tagData={studentTags}
+              tagColors={tagColors}
+            />
+
+            <TagUsageLineChart
+              allSprints={allSprints}
+              allTags={availableTags}
+              selectedTags={selectedTags}
+              tagData={studentTags}
+              tagColors={tagColors}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  state: state,
+  user: {
+    studentNumber: state.login.user.user.student_number,
+    admin: state.login.user.user.admin,
+    instructor: state.login.user.user.instructor,
+    name: `${state.login.user.user.first_name} ${state.login.user.user.last_name}`,
+  },
+  group: state.registrationDetails.myGroup,
+  availableTags: state.tags.availableTags,
+  studentTags: state.tags.studentTags,
+})
+
+const mapDispatchToProps = {
+  setError: notificationActions.setError,
+  setSuccess: notificationActions.setSuccess,
+  fetchAvailableTags: tagsActions.fetchAvailableTags,
+  fetchTagsByStudent: tagsActions.fetchTagsByStudent,
+}
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(StaffTagPage),
+)
