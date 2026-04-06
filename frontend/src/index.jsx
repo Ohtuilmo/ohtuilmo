223edfdb48e40e102ce8fa844827adb07d1701b9
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-06 15:13:46 +0300

    modified theme creation

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './reducers/store'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
 import amber from '@material-ui/core/colors/amber'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#fdd835'
-    },
-    secondary: amber
-  },
-  typography: {
-    useNextVariants: true
-  }
-})
-
 const Root = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  console.log(prefersDarkMode)
+  const dark = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = createTheme({
+    palette: {
+      type: dark ? 'dark' : 'light',
+      primary: {
+        main: '#fdd835',
+      },
+      secondary: amber
+    },
+    typography: {
+      useNextVariants: true
+    },
+  })
 
   return (
     <Provider store={store}>
