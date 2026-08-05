b1b1eac27a5ff6d2eb4080e8f8fa0ee51e01d7f6
Markus <wahlman2002@gmail.com>
2026-08-05 13:36:19 +0300

    chore: update Redux

--- frontend/src/reducers/store.js
+++ frontend/src/reducers/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { thunk } from 'redux-thunk'
 import userHandler from './middleware/userHandler'
 
 // Import all reducers here
